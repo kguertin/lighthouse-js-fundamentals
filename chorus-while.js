e6eb8224e32c22625079b359eb961da98e9dc27b
Kevin Guertin <kevin.m.guertin@gmail.com>
2019-12-04 04:08:21 +0000

    testing while loops

---
+++ chorus-while.js
@@ -0,0 +1,23 @@
+
+//While loops
+/*
+const chorus ='let\'s dance';
+let repeat = 0;
+while(repeat < 10){
+  console.log(chorus);
+  repeat++;
+}
+console.log('Until the sun comes up');
+*/
+
+//While loop w/ conditionals operators
+const chorus = 'let\s dance';
+let repeat = 0;
+while(repeat < 10){
+  if(repeat === 5){
+    console.log('change key');
+  }
+  console.log(chorus);
+  repeat++
+}
+console.log('Until the sun comes up');
