72ad6f1a4e8c72fbbd8fbdf8cf9b44f32a253ae3
Kevin Guertin <kevin.m.guertin@gmail.com>
2019-12-08 19:55:06 +0000

    Learning about functions

---
+++ hello.js
@@ -0,0 +1,12 @@
+//Declaring Functions
+/*
+const sayHello = function(){
+  console.log('Hello, world')
+}
+sayHello();
+*/
+
+const sayHello = function(name){
+  console.log('Hello, ' + name);
+}
+ sayHello('kevin');
