2fe20464718b829e736d41030d137d8cd9da047b
Kevin Guertin <kevin.m.guertin@gmail.com>
2019-12-08 20:15:58 +0000

    learning about using return

---
+++ even.js
@@ -0,0 +1,14 @@
+//Return in functions
+const isEven = function(num){
+  return num % 2 === 0
+}
+//Simplify the code
+/*
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+console.log(isEven(10));
+console.log(isEven(11));
