8dce38f2c5ff82915d5bcea0bcb13bff755c691d
Kevin Guertin <kevin.m.guertin@gmail.com>
2019-12-15 23:45:51 +0000

    for loops and while loops

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,14 @@
+//For loops and arrays
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+//While loops and arrays
+console.log('Kitchen pack to pack:');
+let i = 0;
+while(i < packingList.length){
+  console.log(packingList[i]);
+  i++;
 }
