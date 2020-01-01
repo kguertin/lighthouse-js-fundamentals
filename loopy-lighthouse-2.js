8533bbd88df653ab076dfcec1c07fa90a74d24d5
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-01-01 22:21:33 +0000

    adding unfinished solution to loopy-lighthouse-2

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -1,3 +1,33 @@
 let loopyLighthouse = function(range, multiples, words){
+  let workingArray= [],
+      finalArray = [],
+      tempElm;
 
+  if(range.length > 2 || range.length < 2){
+    return 'Please enter a range of two numbers';
+  }
+
+  for(let i = range[0]; i <= range[1]; i++){
+    workingArray.push(i);
+  }
+
+  for(let i = 0; i < workingArray.length; i++){
+    for(let j = 0; j < multiples.length; j++){
+      if(workingArray[i] % multiples[j] === 0){
+        tempElm += words[j];
+      }
+    }
+    
+    if(tempElm === undefined){
+      finalArray.push(i);
+    }else{
+      finalArray.push(tempElm);
+    }
+
+    tempElm = undefined;
+  }
+  return finalArray;
 }
+
+//Not yet finished, can make it so we can take two of every paramater but code doest work if we introduce another multipul or word.
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
